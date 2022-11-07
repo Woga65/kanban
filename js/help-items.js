9e7a205dcf17baf339e4be0a714721fc83bbc526
Woga65 <woga@mytsa.de>
2022-11-07 04:08:11 +0100

    help text: changes & typos

--- js/help-items.js
+++ js/help-items.js
@@ -1,26 +1,28 @@
 export function helpItems_de() {
     return [
-        `Bewegen Sie Aufgaben und Listen per Drag & drop. Klicken Sie Aufgaben an, um sie zu editiern. Gelöschte Aufgaben werden in den 
-        Papierkorb verschoben.<br><br>Andern Sie Priorität, Fälligkeit oder das zuständige Team Mitglied, indem Sie das jeweilige Feld anklicken.<br><br>
+        `<b>Bewegen Sie Aufgaben und Listen per Drag & drop.</b> Klicken Sie Aufgaben an, um sie zu editiern. Gelöschte Aufgaben werden zunächst in den 
+        Papierkorb verschoben und können von dort aus endgültig gelöscht werden.<br><br><b>Ändern Sie Priorität, Fälligkeit</b> oder das zuständige <b>Team Mitglied</b>, indem Sie das jeweilige Feld anklicken.<br><br>
         <b>Blenden Sie eine Liste aus</b> durch Klicken auf das <span class='icon'>&#xeee1;</span> Icon oben rechts.<br>`,
         `<b>Editieren Sie die Teamzusammenstellung</b> mit einem Klick auf das <span class='icon' style="font-size: 1.3rem;">&#xed0b;<span> icon.`,
         `<b>Editieren Sie die Prioritätenliste</b> nach einem Klick auf das <img style="height: 2rem; object-position: 0 0.35rem;" src="img/priority.svg"> icon.`,
-        `<b>Fügen Sie listen hinzu oder löschen Listen,</b> indem Sie auf die entsprechenden Icons klicken. Wird eine Liste gelöscht, werden die ihr 
+        `<b>Fügen Sie Listen hinzu oder löschen Listen,</b> indem Sie auf die entsprechenden Icons klicken. Wird eine Liste gelöscht, werden die ihr 
         zugeordneten Aufgaben in den Papierkorb verschoben.`,
-        "<b>Blenden Sie das Backlog ein oder aus</b> mit einem Klick auf das <span class='icon'>&#xead1;</span> Icon.<br><br>",
+        "<b>Blenden Sie das Backlog ein oder aus</b> mit einem Klick auf das <span class='icon'>&#xead1;</span> Icon.",
+        `<b>Dasselbe gilt auch für den Papierkorb,</b> den Sie mit einem Klick auf das <span class="icon" style="font-family: 'LineAwesome'; font-size: 1.5rem;">&#xf2ed;</span> Icon ein- und wieder ausblenden können.<br><br>`,
     ];
 }
 
 
 export function helpItems_en() {
     return [
-        `Drag & drop tasks and lists as you wish. Edit a task by clicking the task's body. Deleted tasks will be moved 
-        to the trash folder.<br><br>Change the priority, due date or team member a task is assigned to by simply clicking on it.<br><br>
+        `<b>Drag & drop tasks and lists as you wish.</b> Edit a task by clicking the task's body. Deleted tasks will be moved 
+        to the trash folder.<br><br><b>Change the priority, due date or team member</b> a task is assigned to by simply clicking on it.<br><br>
         <b>Hide a list</b> by clicking the <span class='icon'>&#xeee1;</span> Icon at the top-right corner of the list.<br>`,
         `<b>Add / Remove team members</b> by clicking the <span class='icon' style="font-size: 1.3rem;">&#xed0b;<span> icon.`,
         `<b>Edit the priorities list</b> by clicking the <img style="height: 2rem; object-position: 0 0.35rem;" src="img/priority.svg"> icon.`,
         `<b>Add / Remove lists</b> by clicking the appropiate icons. If a list is removed, the related tasks 
         will be moved to the trash folder.`,
-        "<b>Show / hide the backlog</b> by clicking the <span class='icon'>&#xead1;</span> Icon.<br><br>",
+        "<b>Show / hide the backlog</b> by clicking the <span class='icon'>&#xead1;</span> Icon.",
+        `<b>Show / hide the trash can</b> by clicking the <span class="icon" style="font-family: 'LineAwesome'; font-size: 1.5rem;">&#xf2ed;</span> Icon.<br><br>`,
     ];
 }
